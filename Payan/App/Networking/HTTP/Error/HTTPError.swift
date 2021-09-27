cf49536930d05b10011c67c1d2d7edca2f37e1d5
juandahurt <juandahurtadom@gmail.com>
2021-09-26 23:24:07 -0500

    feat: added HTTPHelper and HTTPError

---
+++ Payan/App/Networking/HTTP/Error/HTTPError.swift
@@ -0,0 +1,12 @@
+//
+//  HTTPError.swift
+//  Payan
+//
+//  Created by juandahurt on 26/09/21.
+//
+
+import Foundation
+
+enum HTTPError: Error {
+    case malformedUrl
+}
--- Payan/App/Services/AuthService.swift
+++ Payan/App/Services/AuthService.swift
@@ -13,38 +13,33 @@ protocol AnyAuthService {
 }
 
 final class RESTAPIAuthService: AnyAuthService {
-    
     func login(with credential: Credential) -> Single<String> {
-        let endpoint = HTTPAuthEndpoint.login
-        let url = URL(string: endpoint.path)!
-        var request = URLRequest(url: url)
-        request.addValue("Application/json", forHTTPHeaderField: "Accept")
-        request.addValue("Application/json", forHTTPHeaderField: "Content-Type")
-        request.httpMethod = endpoint.method.rawValue
-        request.httpBody = try! JSONEncoder().encode(credential)
-        Console.log("app is about to call: \(endpoint.path)", level: .network)
-        
-        return Single.create { single in
-            URLSession.shared.dataTask(with: request) { data, response, error in
-                if let error = error {
-                    Console.log("\(error)", level: .error)
-                    single(.failure(error))
-                }
-                if let data = data {
-                    do {
-                        let decodedBody = try JSONDecoder().decode(HTTPServerResponse<String>.self, from: data)
-                        Console.log("decoded server response: \(decodedBody)", level: .network)
-                        if decodedBody.success {
-                            single(.success(decodedBody.data!))
-                        } else {
-                            single(.failure(decodedBody.error!))
-                        }
-                    } catch {
+        Single.create { single in
+            let endpoint = HTTPAuthEndpoint.login
+            if let request = HTTPHelper.createRequest(using: endpoint, data: credential) {
+                URLSession.shared.dataTask(with: request) { data, response, error in
+                    if let error = error {
                         Console.log("\(error)", level: .error)
                         single(.failure(error))
                     }
-                }
-            }.resume()
+                    if let data = data {
+                        do {
+                            let decodedBody = try JSONDecoder().decode(HTTPServerResponse<String>.self, from: data)
+                            Console.log("decoded server response: \(decodedBody)", level: .network)
+                            if decodedBody.success {
+                                single(.success(decodedBody.data!))
+                            } else {
+                                single(.failure(decodedBody.error!))
+                            }
+                        } catch {
+                            Console.log("\(error)", level: .error)
+                            single(.failure(error))
+                        }
+                    }
+                }.resume()
+            } else {
+                single(.failure(HTTPError.malformedUrl))
+            }
             return Disposables.create()
         }
     }
