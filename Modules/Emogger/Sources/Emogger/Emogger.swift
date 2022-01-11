80e7334a9239c75a0c2323ddfe40a5f75ff86466
juandahurt <juandahurtadom@gmail.com>
2022-01-10 20:00:48 -0500

    refactor: added log validation

--- Modules/Emogger/Sources/Emogger/Emogger.swift
+++ Modules/Emogger/Sources/Emogger/Emogger.swift
@@ -1,5 +1,6 @@
 public struct Emogger {
     private let levels: [LogLevel]
+    private(set) var numberOfLogs = 0
     
     public init(levels: [LogLevel]) throws {
         guard !levels.isEmpty else {
@@ -9,7 +10,16 @@ public struct Emogger {
         self.levels = levels
     }
     
-    public func log(_ message: String, level: LogLevel) {
+    public mutating func log(_ message: String, level: LogLevel) {
+        guard isLevelEnabled(level) else {
+            return
+        }
+        
         print("\(level.emoji) \(message)")
+        numberOfLogs += 1
+    }
+    
+    private func isLevelEnabled(_ level: LogLevel) -> Bool {
+        levels.contains(level)
     }
 }
