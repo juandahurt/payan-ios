5314e0992c16c3a09e1b2c5d290e396c04259a70
juandahurt <juandahurtadom@gmail.com>
2022-01-10 19:47:38 -0500

    refactor: added log levels

--- Modules/Emogger/Sources/Emogger/Emogger.swift
+++ Modules/Emogger/Sources/Emogger/Emogger.swift
@@ -1,8 +1,12 @@
 public struct Emogger {
+    private let levels: [LogLevel]
+    
     public init(levels: [LogLevel]) throws {
         guard !levels.isEmpty else {
             throw EmoggerError.emptyLevels
         }
+        
+        self.levels = levels
     }
     
     public func log(_ message: String, level: LogLevel) {
--- Modules/Emogger/Sources/Emogger/LogLevel.swift
+++ Modules/Emogger/Sources/Emogger/LogLevel.swift
@@ -10,9 +10,17 @@ import Foundation
 public struct LogLevel {
     var emoji: String
     
-    public static let debug = LogLevel(emoji: "")
+    public static let debug = LogLevel(emoji: "🐛")
+    public static let warning = LogLevel(emoji: "⚠️")
+    public static let info = LogLevel(emoji: "💬")
+    public static let network = LogLevel(emoji: "🌐")
+    public static let event = LogLevel(emoji: "📲")
     
     public static let all: [LogLevel] = [
-        .debug
+        .debug,
+        .warning,
+        .info,
+        .network,
+        .event
     ]
 }
