4ebdd2933295d0438aeb59e64e4bd7406bd915cd
Juan David Hurtado <juandahurtadom@gmail.com>
2022-04-12 17:16:07 -0500

    refactor: remove dummy data in app version

--- Payan/App/Domain Modules/Home/Interactor/PYHInteractor.swift
+++ Payan/App/Domain Modules/Home/Interactor/PYHInteractor.swift
@@ -21,11 +21,12 @@ final class PYHInteractor: PYHBusinessLogic {
         presenter.showLoading()
         worker.getLastVersion { [weak self] version in
             guard let self = self else { return }
-            
-            self.presenter.hideLoading()
-            if let currVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String {
-                if currVersion < version.number {
-                    self.presenter.showAppNeedsUpdate(version.type)
+            DispatchQueue.main.async {
+                self.presenter.hideLoading()
+                if let currVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String {
+                    if currVersion < version.number {
+                        self.presenter.showAppNeedsUpdate(version.type)
+                    }
                 }
             }
         }
