b3d363a8a1e02de53603f759074206eb7988eec3
Juan David Hurtado <juandahurtadom@gmail.com>
2022-04-24 15:25:17 -0500

    feat: add network manager

---
+++ Payan/App/Core/Networking/PYNetworkError.swift
@@ -0,0 +1,12 @@
+//
+//  PYNetworkError.swift
+//  Payan
+//
+//  Created by Juan Hurtado on 24/04/22.
+//
+
+import Foundation
+
+enum PYNetworkError: Error {
+    case malformedUrl
+}
---
+++ Payan/App/Core/Networking/PYNetworkRequest.swift
@@ -0,0 +1,13 @@
+//
+//  PYNetworkRequest.swift
+//  Payan
+//
+//  Created by Juan Hurtado on 24/04/22.
+//
+
+import Foundation
+
+struct PYNetworkRequest {
+    var endpoint: String
+    var method: PYNetworkMethod = .get
+}
