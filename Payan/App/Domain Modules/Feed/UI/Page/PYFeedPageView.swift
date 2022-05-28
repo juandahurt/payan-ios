2b09a58976b3d1991af4e2cf131cf1b7e20038f9
Juan David Hurtado <juandahurtadom@gmail.com>
2022-05-28 11:18:04 -0500

    refactor: upgrade card collection view

--- Payan/App/Domain Modules/Feed/UI/Page/PYFeedPageView.swift
+++ Payan/App/Domain Modules/Feed/UI/Page/PYFeedPageView.swift
@@ -51,7 +51,11 @@ struct PYFeedPageView: View {
                 PuraceTextView("Próceres", fontSize: 20, textColor: PuraceStyle.Color.N1)
                 PuraceTextView("Algunos personajes ilustres de la ciudad", fontSize: 14, textColor: PuraceStyle.Color.N4)
             }.padding(.bottom)
-            PuraceCollectionCardView(cards: viewModel.feedData.heroes)
+            PuraceCollectionCardView(
+                firstCardSize: .init(width: UIScreen.main.bounds.width * 0.6, height: UIScreen.main.bounds.width * 0.7),
+                cards: viewModel.feedData.heroes
+            )
+                .frame(height: UIScreen.main.bounds.width * 0.7)
                 .padding(.bottom)
             PuraceButtonView("Ver todos", fontSize: 14, type: .quiet) {
                 guard let url = URL(string: "payan://collection?type=hero") else { return }
