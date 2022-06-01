b54e6d329d2cb65cf98daa75588113d8cec972a8
Juan David Hurtado <juandahurtadom@gmail.com>
2022-06-01 12:05:10 -0500

    refactor: add place ui improvements

--- Payan/App/Domain Modules/Place/UI/Page/PYPlacePageView.swift
+++ Payan/App/Domain Modules/Place/UI/Page/PYPlacePageView.swift
@@ -11,11 +11,14 @@ import Purace
 
 struct PYPlacePageView: View, PYPlaceViewLogic {
     var placeId: String
-    
+    let topSafeAreaPadding: CGFloat
     @StateObject var viewModel = PYPlaceViewModel()
+    @State var descriptionHeight: CGFloat = .zero
     
     init(placeId: String) {
         self.placeId = placeId
+        let window = UIApplication.shared.windows.filter {$0.isKeyWindow}.first
+        topSafeAreaPadding = window?.safeAreaInsets.top ?? .zero
     }
     
     var navBar: some View {
@@ -29,6 +32,7 @@ struct PYPlacePageView: View, PYPlaceViewLogic {
             Spacer()
         }.padding()
             .frame(height: 50)
+            .padding(.top, topSafeAreaPadding)
     }
     
     var image: some View {
@@ -36,15 +40,15 @@ struct PYPlacePageView: View, PYPlaceViewLogic {
         return ZStack {
             PuraceImageView(url: url)
                 .aspectRatio(contentMode: .fill)
-                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.35)
+                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.4)
                 .clipped()
                 .skeleton(with: viewModel.isLoading)
                 .shape(type: .rectangle)
                 .animation(type: .none)
                 .appearance(type: .solid())
-            LinearGradient(colors: [.black.opacity(0.35), .clear], startPoint: .top, endPoint: .bottom)
+            LinearGradient(colors: [.black.opacity(0.4), .clear], startPoint: .top, endPoint: .center)
         }
-            .frame(height: UIScreen.main.bounds.height * 0.35)
+        .frame(height: UIScreen.main.bounds.height * 0.4)
     }
     
     var title: some View {
@@ -83,7 +87,7 @@ struct PYPlacePageView: View, PYPlaceViewLogic {
     }
     
     var description: some View {
-        PuraceTextView(viewModel.place.description ?? "")
+        DescriptionView(text: viewModel.place.description ?? "")
             .skeleton(with: viewModel.isLoading)
             .multiline(lines: 5, scales: [4: 0.5])
             .animation(type: .none)
@@ -108,9 +112,44 @@ struct PYPlacePageView: View, PYPlaceViewLogic {
                 Spacer()
             }
         }
+        .edgesIgnoringSafeArea(.top)
         .navigationBarHidden(true)
         .onFirstAppear {
             viewModel.getPlace(id: placeId)
+        }
+    }
+    
+    struct DescriptionView: View {
+        var text: String
+
+        @State private var height: CGFloat = .zero
+
+        var body: some View {
+            InternalLabelView(text: text, dynamicHeight: $height)
+                .frame(minHeight: height)
+        }
+
+        struct InternalLabelView: UIViewRepresentable {
+            var text: String
+            @Binding var dynamicHeight: CGFloat
+
+            func makeUIView(context: Context) -> UILabel {
+                let label = UILabel()
+                label.numberOfLines = 0
+                label.textAlignment = .justified
+                label.lineBreakMode = .byWordWrapping
+                label.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+                label.textColor = UIColor(PuraceStyle.Color.N1)
+                label.font = .systemFont(ofSize: 12)
+                return label
+            }
+
+            func updateUIView(_ uiView: UILabel, context: Context) {
+                uiView.text = text
+                DispatchQueue.main.async {
+                    dynamicHeight = uiView.sizeThatFits(CGSize(width: uiView.bounds.width, height: CGFloat.greatestFiniteMagnitude)).height
+                }
+            }
         }
     }
 }
