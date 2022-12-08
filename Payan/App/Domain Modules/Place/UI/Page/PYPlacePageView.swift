38e96c94bf5abce109adb2d3825479f045d859b6
Juan David Hurtado <juandahurtadom@gmail.com>
2022-12-08 17:51:38 -0500

    feat: add place sticky image

--- Payan/App/Domain Modules/Place/UI/Page/PYPlacePageView.swift
+++ Payan/App/Domain Modules/Place/UI/Page/PYPlacePageView.swift
@@ -9,6 +9,7 @@ import Foundation
 import MapKit
 import SwiftUI
 import Purace
+import Kingfisher
 
 struct PYPlacePageView: View, PYPlaceViewLogic {
     var placeId: String
@@ -42,14 +43,18 @@ struct PYPlacePageView: View, PYPlaceViewLogic {
     
     var image: some View {
         let url = URL(string: viewModel.place.image)
-        return PuraceImageView(url: url)
-            .scaledToFill()
-            .frame(height: UIScreen.main.bounds.height * 0.3)
-            .clipped()
-            .animation(.none)
-            .skeleton(with: viewModel.isLoading)
-            .shape(type: .rectangle)
-            .frame(height: UIScreen.main.bounds.height * 0.3)
+        return GeometryReader { reader in
+            let minY = reader.frame(in: .named("scroll")).minY
+            
+            return PuraceImageView(url: url)
+                .aspectRatio(contentMode: .fill)
+                .frame(width: reader.size.width, height: reader.size.height + (minY > 0 ? minY : 0), alignment: .top)
+                .clipped()
+                .animation(.none)
+                .skeleton(with: viewModel.isLoading)
+                .shape(type: .rectangle)
+                .offset(x: 0, y: minY > 0 ? -minY : 0)
+        }.frame(height: UIScreen.main.bounds.height * 0.3)
     }
     
     var title: some View {
@@ -103,10 +108,7 @@ struct PYPlacePageView: View, PYPlaceViewLogic {
                         .clipped()
                         .contentShape(Rectangle())
                         .onTapGesture {
-                            selectedImage = viewModel.place.images.count - index - 1
-                            withAnimation {
-                                selectedImageIsVisible = true
-                            }
+                            
                         }
                 }
             }.frame(height: UIScreen.main.bounds.height * 0.55)
@@ -114,7 +116,7 @@ struct PYPlacePageView: View, PYPlaceViewLogic {
     }
     
     var body: some View {
-        VStack {
+        VStack(alignment: .center) {
             navBar
             
             ScrollView(showsIndicators: false) {
@@ -129,7 +131,7 @@ struct PYPlacePageView: View, PYPlaceViewLogic {
                     }
                     Spacer(minLength: 0)
                 }
-            }
+            }.coordinateSpace(name: "scroll")
         }
         .snackBar(title: "Parece que ha habido un error", isVisible: $viewModel.errorHasOccured, type: .error, buttonTitle: "REINTENTAR")
         .onChange(of: viewModel.errorHasOccured) { value in
