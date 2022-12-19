3780bd5a3e7453c9cde5aa2253ed7a2ec6c23844
Juan David Hurtado <juandahurtadom@gmail.com>
2022-12-19 17:39:04 -0500

    feat: update about page ui

---
+++ Payan/App/Domain Modules/About/Models/PYAboutConstants.swift
@@ -0,0 +1,14 @@
+//
+//  PYAboutConstants.swift
+//  Payan
+//
+//  Created by Juan Hurtado on 18/12/22.
+//
+
+import Foundation
+
+struct PYAboutConstants {
+    struct Analytics {
+        static let pageName = "/about"
+    }
+}
--- Payan/App/Domain Modules/About/UI/Page/PYAboutPageView.swift
+++ Payan/App/Domain Modules/About/UI/Page/PYAboutPageView.swift
@@ -14,55 +14,39 @@ struct PYAboutPageView: View {
         Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? ""
     }
     
-    private var buildNumber: String {
-        Bundle.main.infoDictionary?["CFBundleVersion"] as? String ?? ""
-    }
-    
-    var navBar: some View {
-        HStack(alignment: .center) {
-            Image(systemName: "chevron.left")
-                .foregroundColor(PuraceStyle.Color.N1)
-                .scaleEffect(1.2)
-                .padding()
-                .background(Color.black.opacity(0.001))
-                .onTapGesture {
-                    PYRoutingManager.shared.pop()
-                }
-            Spacer()
-            PuraceTextView("Acerca de", fontSize: 16, weight: .medium)
-            Spacer()
-            Image(systemName: "chevron.left")
-                .foregroundColor(PuraceStyle.Color.N1)
-                .scaleEffect(1.2)
-                .opacity(0)
-                .padding()
-        }
-            .frame(height: 50)
-            .padding(.bottom, 50)
-    }
-    
     var description: some View {
         PuraceAccordionView(title: "¿Qué es Payán?") {
             PuraceTextView(
                 "Payán es un proyecto sin ánimo de lucro cuyo principal objetivo es mostrar al mundo al belleza e historia que esconde la ciudad de Popayán, Colombia.",
                 textColor: PuraceStyle.Color.N2
             ).padding()
+                .background(Color.white)
         }
+        .cornerRadius(10)
+        .padding(.horizontal, 16)
     }
     
     var title: some View {
-        VStack {
-            PuraceTextView("Payan en iOS", fontSize: 14, weight: .medium)
-            PuraceTextView("Versión \(version)-\(buildNumber)", fontSize: 10, textColor: PuraceStyle.Color.N4)
-        }.padding(.bottom)
+        VStack(spacing: 22) {
+            icon
+            
+            VStack(spacing: 12) {
+                PuraceTextView("Payan en iOS", fontSize: 18, weight: .medium)
+                PuraceTextView("Versión \(version)")
+            }
+        }.padding(.top, 50)
     }
     
     var icon: some View {
         Image("app")
             .resizable()
             .scaledToFit()
-            .frame(height: 80)
-            .padding(.bottom, 20)
+            .frame(height: 70)
+            .padding(.vertical, 15)
+            .padding(.horizontal, 25)
+            .background(Color.white)
+            .cornerRadius(20)
+            .shadow(color: .black.opacity(0.05), radius: 4, y: 4)
     }
     
     var divider: some View {
@@ -71,13 +55,28 @@ struct PYAboutPageView: View {
             .padding(.vertical, 25)
     }
     
-    var body: some View {
+    var footer: some View {
         VStack {
-            navBar
-            icon
-            title
-            description
-            Spacer()
-        }.navigationBarHidden(true)
+            PuraceTextView("Hecho en Popayán, Colombia.", fontSize: 10, textColor: .black.opacity(0.3))
+            PuraceTextView("2022", fontSize: 10, textColor: .black.opacity(0.3))
+        }.padding(.bottom)
+    }
+    
+    var body: some View {
+        PuraceScaffold(navBar: .init(title: "Acerca de", backOnTap: {
+            PYRoutingManager.shared.pop()
+        })) {
+            VStack(spacing: 35) {
+                title
+                description
+                Spacer()
+                footer
+            }
+        }.background(PuraceStyle.Color.F1)
+            .onAppear {
+                AnalyticsManager.shared.trackView(
+                    path: PYAboutConstants.Analytics.pageName
+                )
+            }
     }
 }
